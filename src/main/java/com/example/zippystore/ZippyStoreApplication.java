db649903187292b53c3024777b6448a7bd88ee89
Naveen Anantapalli <anaveen2000@gmail.com>
2021-04-20 17:49:41 +0530

    test

---
+++ src/main/java/com/example/zippystore/ZippyStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.zippystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZippyStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZippyStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zippystore/ZippyStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zippystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZippyStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
