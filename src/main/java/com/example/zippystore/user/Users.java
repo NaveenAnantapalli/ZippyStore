4c1c6c481bdd3afcb55395f25d2bb77e14a55e91
sowmyamedidi <dsmedidi@gmail.com>
2021-05-06 22:53:46 +0530

    User table created in Users.java

--- src/main/java/com/example/zippystore/user/Users.java
+++ src/main/java/com/example/zippystore/user/Users.java
@@ -1,4 +1,110 @@
 package com.example.zippystore.user;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Date;
+
+@Entity
+@Table(name = "tblUser")
 public class Users {
+
+    @Id
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence"
+    )
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "user_email")
+    private String userEmail;
+
+    @Column(name = "user_phone_number")
+    private Long userPhoneNumber;
+
+    @Column(name = "user_address")
+    private String userAddress;
+
+    @Column(name = "user_dob")
+    private LocalDate userDOB;
+
+    @Transient
+    private Integer age;
+
+    public Users() {
+    }
+
+    public Users(Long userId, String userName, String userEmail, Long userPhoneNumber, String userAddress, LocalDate userDOB) {
+        this.userId = userId;
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userPhoneNumber = userPhoneNumber;
+        this.userAddress = userAddress;
+        this.userDOB = userDOB;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public Long getUserPhoneNumber() {
+        return userPhoneNumber;
+    }
+
+    public void setUserPhoneNumber(Long userPhoneNumber) {
+        this.userPhoneNumber = userPhoneNumber;
+    }
+
+    public String getUserAddress() {
+        return userAddress;
+    }
+
+    public void setUserAddress(String userAddress) {
+        this.userAddress = userAddress;
+    }
+
+    public LocalDate getUserDOB() {
+        return userDOB;
+    }
+
+    public void setUserDOB(LocalDate userDOB) {
+        this.userDOB = userDOB;
+    }
+
+    public Integer getAge() {
+        return Period.between(this.userDOB, LocalDate.now()).getYears();
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
