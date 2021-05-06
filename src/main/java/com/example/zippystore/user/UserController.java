63ce0be8e56f0c9a64a8f80ee9ead2ecb4e96637
sowmyamedidi <dsmedidi@gmail.com>
2021-05-06 15:31:34 +0530

    modified db name and created the user controller with sample functionality

---
+++ src/main/java/com/example/zippystore/user/UserController.java
@@ -0,0 +1,14 @@
+package com.example.zippystore.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/user")
+public class UserController {
+
+     @RequestMapping(path = "/userlogin")
+    public String testMethod(){
+        return "Hello World!";
+     }
+}
